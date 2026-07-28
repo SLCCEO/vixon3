983f8bf570aaaf4b7ab09a093de83285c5ab4481
Jeremiah_YT <jeremiahmhass@gmail.com>
2026-07-28 15:59:55 -0700

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+{
+  "compilerOptions": {
+    "target": "ES2020",
+    "useDefineForClassFields": true,
+    "lib": ["ES2020", "DOM", "DOM.Iterable"],
+    "module": "ESNext",
+    "skipLibCheck": true,
 
-export default defineConfig({
-  plugins: [react()],
-})
+    /* Bundler mode */
+    "moduleResolution": "bundler",
+    "allowImportingTsExtensions": true,
+    "isolatedModules": true,
+    "moduleDetection": "force",
+    "noEmit": true,
+    "jsx": "react-jsx",
+
+    /* Linting */
+    "strict": true,
+    "noUnusedLocals": true,
+    "noUnusedParameters": true,
+    "noFallthroughCasesInSwitch": true
+  },
+  "include": ["src"]
+}
